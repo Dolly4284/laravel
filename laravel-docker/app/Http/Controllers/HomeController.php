507124e12fe97aea3edd06bc3c8d110519ac1e99
Dolly4284 <djean.pulgarinas@gmail.com>
2024-08-31 06:27:22 +0000

    Second Model, GET Parameters and Eloquent 01

--- laravel-docker/app/Http/Controllers/HomeController.php
+++ laravel-docker/app/Http/Controllers/HomeController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Category;
+use App\Models\Post;    
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $allCategories = Category::all();  
-        return view('home', ['categories' => $allCategories]); 
+        $categories = Category::all();
+        $posts = Post::latest()->get();
+
+        return view('home', compact('categories', 'posts')); 
+       
     }
 }   
