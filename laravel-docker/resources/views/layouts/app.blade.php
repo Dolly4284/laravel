2ce7d4ca5e24557c78aefdf2b56883c708fa9630
Dolly4284 <djean.pulgarinas@gmail.com>
2024-08-31 04:05:15 +0000

    DB Structure, Migrations and .env Config

--- laravel-docker/resources/views/layouts/app.blade.php
+++ laravel-docker/resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="https://musical-space-pancake-q7954w67p5qpc4qqv-80.app.github.dev/css/styles.css" rel="stylesheet" />
+        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" /> 
     </head>
     <body>
         <!-- Responsive navbar-->
@@ -19,9 +19,9 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('home') }}">Home</a></li> 
+                    <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('contact') }}">Contact</a></li>
                         <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Blog</a></li>
                     </ul>
                 </div>
@@ -37,6 +37,6 @@
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
-        <script src="https://musical-space-pancake-q7954w67p5qpc4qqv-80.app.github.dev/js/scripts.js"></script>
+        <script src="{{ asset('js/scripts.js') }}"></script> 
     </body>
 </html>
