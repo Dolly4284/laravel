417621f7ee4e0716767c46b85c0dd7be8fa6cd6e
Dolly4284 <djean.pulgarinas@gmail.com>
2024-08-31 05:49:22 +0000

    MVC: Model, Controller and View with Foreach

--- laravel-docker/routes/web.php
+++ laravel-docker/routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
-Route::view('/', 'home')->name('home');
+$url = config('app.url');
+URL::forceRootUrl($url);
+
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::view('contact', 'contact')->name('contact');
 Route::view('about', 'about')->name('about');
 
